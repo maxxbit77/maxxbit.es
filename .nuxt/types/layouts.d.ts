a3e77f68f7e3175ba0bc182a521237ff70e57c5b
maxielcade04 <maxielcade04@gmail.com>
2025-01-17 19:00:45 +0100

    testing for solving development bug

--- .nuxt/types/layouts.d.ts
+++ .nuxt/types/layouts.d.ts
@@ -1,6 +1,6 @@
-import type { ComputedRef, MaybeRef } from 'vue'
+import { ComputedRef, MaybeRef } from 'vue'
 export type LayoutKey = string
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "../../node_modules/.pnpm/nuxt@3.9.3_eslint@8.56.0_vite@5.0.12/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
   }
--- .nuxt/types/middleware.d.ts
+++ .nuxt/types/middleware.d.ts
@@ -1,12 +1,7 @@
 import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = never
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
+export type MiddlewareKey = string
+declare module "../../node_modules/.pnpm/nuxt@3.9.3_eslint@8.56.0_vite@5.0.12/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
-declare module 'nitropack' {
-  interface NitroRouteConfig {
-    appMiddleware?: MiddlewareKey | MiddlewareKey[] | Record<MiddlewareKey, boolean>
   }
 }
